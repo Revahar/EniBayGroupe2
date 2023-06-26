127753e3569d9768bc82bef5d79c9aa5935cc422
Michaud Nathanael <nath.michaud@hotmail.fr>
2023-06-26 10:10:00 +0200

    initial commit

---
+++ EniBay/src/main/java/fr/eni/EniBay/EniBayApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.EniBay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniBayApplication.class, args);
+	}
+
+}
---
+++ EniBay/src/test/java/fr/eni/EniBay/EniBayApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.EniBay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniBayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
