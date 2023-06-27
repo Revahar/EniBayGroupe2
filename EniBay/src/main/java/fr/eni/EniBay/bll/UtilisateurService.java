60d9b06e1c198a7671ef49508b3840ded04961b8
Noroy-afk <56821694+Noroy-afk@users.noreply.github.com>
2023-06-27 15:19:00 +0200

    + utilisateur service

---
+++ EniBay/src/main/java/fr/eni/EniBay/bll/UtilisateurService.java
@@ -0,0 +1,13 @@
+package fr.eni.EniBay.bll;
+
+import java.util.List;
+
+import fr.eni.EniBay.bo.Utilisateur;
+
+public interface UtilisateurService {
+	List<Utilisateur> getUtilisateurs();
+
+	void ajouterUtilisateur(Utilisateur utilisateur);
+
+	void afficher(Utilisateur utilisateur);
+}
