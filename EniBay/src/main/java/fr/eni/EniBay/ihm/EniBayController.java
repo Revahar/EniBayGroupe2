559b30505a09bc8a9991161c77a7bee1bf8c6c35
blutch97 <82734602+blutch97@users.noreply.github.com>
2023-07-06 16:39:54 +0200

    Validation Nouvelle Enchère

--- EniBay/src/main/java/fr/eni/EniBay/ihm/EniBayController.java
+++ EniBay/src/main/java/fr/eni/EniBay/ihm/EniBayController.java
@@ -233,8 +233,13 @@ public class EniBayController {
 	}
 	
 	@PostMapping("/enregistrer-nouvelle-vente")
-    public String enregistrerNouvelleVente(@ModelAttribute ArticleVendu article, Retrait retrait, Principal principal) { //@RequestParam("imageFile") MultipartFile imageFile sert à importer des images, à ne pas utiliser pour le moment
+    public String enregistrerNouvelleVente(@Valid @ModelAttribute ArticleVendu article, BindingResult bindingResult, Retrait retrait, Principal principal) { //@RequestParam("imageFile") MultipartFile imageFile sert à importer des images, à ne pas utiliser pour le moment
         System.out.println("enregistrer nouvelle vente");
+        
+        if(bindingResult.hasErrors()) {
+        	return "redirect:/nouvelle-vente";
+        }
+        
         var utilisateur = utilisateurService.findByName(principal.getName());
         do
             articleVenduService.ajouterArticleVendu(article, utilisateur);
